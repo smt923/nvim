d8f5ada5f165c271ebf7301ee432a652e5722b69
smt923 <smt@tilde.club>
2024-11-02 20:06:00 +0000

    wsl clipboard junk

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,38 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Use the right clipboard when on WSL
+local function is_wsl()
+  local wsl_check = os.getenv("WSL_DISTRO_NAME") ~= nil
+  return wsl_check
+end
+
+if is_wsl() then
+  -- vim.g.clipboard = "" -- responsive but no sync to windows thru wsl :(
+  vim.g.clipboard = {
+    name = "win32yank-wsl",
+    copy = {
+      ["+"] = "win32yank.exe -i --crlf",
+      ["*"] = "win32yank.exe -i --crlf",
+    },
+
+    paste = {
+      ["+"] = "win32yank.exe -o --lf",
+      ["*"] = "win32yank.exe -o --lf",
+    },
+    cache_enabled = 1,
+  }
+
+  -- sync with system clipboard on focus
+  vim.api.nvim_create_autocmd({ "FocusGained" }, {
+    pattern = { "*" },
+    command = [[call setreg("@", getreg("+"))]],
+  })
+
+  -- sync with system clipboard on focus
+  vim.api.nvim_create_autocmd({ "FocusLost" }, {
+    pattern = { "*" },
+    command = [[call setreg("+", getreg("@"))]],
+  })
+end
